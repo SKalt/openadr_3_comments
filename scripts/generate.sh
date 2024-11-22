52023459beea054d6069d84c62d53f4473a76c95
Steven Kalt <kalt.steven@gmail.com>
2024-11-22 07:33:53 -0500

    suggestion: split RI into its own file

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,20 +1,46 @@
 #!/bin/sh
+set -eu
 _lang=${1:-${OUTPUT_LANG:-go}}
-rm -rf "out/$_lang"
 
+rm -rf "out/spec/$_lang"
+rm -rf "out/ri/$_lang"
+echo '*' > "out/.gitignore"
+
+echo "spec::openapi-codegen ###################################"
+echo
 docker run --rm \
     -v "$PWD:/local" \
     docker.io/openapitools/openapi-generator-cli generate \
     -i /local/1_oadr3.0.1.yaml \
     -g "$_lang" \
-    -o "/local/out/$_lang"
+    -o "/local/out/spec/$_lang/openapi-codegen"
 
-# swagger-codegen seems to think this is a JSON file.
-# docker run --rm \
-#     -v "$PWD:/local" \
-#     docker.io/swaggerapi/swagger-codegen-cli generate \
-#     -i /local/1_oadr3.0.1.yaml \
-#     -l "$_lang" \
-#     -o "/local/out/$_lang"
+echo
+echo "spec::swagger-codegen ###################################"
+echo
+docker run --rm \
+  -v "$PWD:/local" \
+  docker.io/swaggerapi/swagger-codegen-cli-v3 generate \
+  -i /local/1_oadr3.0.1.yaml \
+  -l "$_lang" \
+  -o "/local/out/spec/$_lang/swagger-codegen"
 
-echo '*' > "out/$_lang/.gitignore"
+echo
+echo "ri::openapi-codegen ###################################"
+echo
+docker run --rm \
+  -v "$PWD:/local" \
+  docker.io/openapitools/openapi-generator-cli generate \
+  -i /local/ri.yaml \
+  -g "$_lang" \
+  -o "/local/out/ri/$_lang/openapi-codegen"
+
+echo
+echo "ri::swagger-codegen ###################################"
+echo
+docker run --rm \
+  -v "$PWD:/local" \
+  docker.io/swaggerapi/swagger-codegen-cli-v3 generate \
+  -i /local/ri.yaml \
+  -l "$_lang" \
+  -o "/local/out/ri/$_lang/swagger-codegen"
